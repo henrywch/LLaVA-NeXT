5d6ae8dfe01dca0ddc3b4963bbf302038381c685
henrywch <2462977089@qq.com>
2025-07-30 14:09:20 +0800

    Unzip script path changed.

--- scripts/train/unzip.sh
+++ scripts/train/unzip.sh
@@ -1,2 +1,2 @@
-find /mnt/LLaVA_Train/ -maxdepth 2 -type f -name "*.zip" -print0 | parallel -0 --bar "sh -c 'dest=\$(echo {} | sed -e \"s|^/mnt/LLaVA_Train|/mnt/datasets/LLaVA_Train|\" -e \"s|\\.zip\$||\"); mkdir -p \"\$dest\"; unzip -qo {} -d \"\$dest\"'"
+find /mnt/LLaVA_Train/ -maxdepth 2 -type f -name "*.zip" -print0 | parallel -0 --bar "sh -c 'dest=\$(echo {} | sed -e \"s|^/mnt/LLaVA_Train|/data/datasets/LLaVA_Train|\" -e \"s|\\.zip\$||\"); mkdir -p \"\$dest\"; unzip -qo {} -d \"\$dest\"'"
 
